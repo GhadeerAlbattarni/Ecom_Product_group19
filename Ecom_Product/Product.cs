f4817ce6f1ab9422c6ba5756df01d238ded493ab
GhadeerAlbattarni <ghadeeralbattarni@gmail.com>
2024-06-14 11:25:05 -0400

    Second commit : containing the completed Product class

---
+++ Ecom_Product/Product.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace Ecom_Product
+{
+	public class Product
+	{
+        // Properties
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+
+        // Constructor
+        public Product(int productId, string productName, decimal price, int stock)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            Price = price;
+            Stock = stock;
+        }
+
+        // Method to increase stock
+        public void StockIncrement(int amount)
+        {
+            if (amount > 0)
+            {
+                Stock += amount;
+            }
+            else
+            {
+                Stock += 0;
+              
+            }
+        }
+
+        // Method to decrease stock
+        public void StockDecrement(int amount)
+        {
+            if (amount > 0)
+            {
+                Stock -= amount;
+            }
+            else
+            {
+                Stock -= 0;
+                
+            }
+        }
+    }
+}
+
